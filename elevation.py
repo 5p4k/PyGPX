4d08ee6276c84c02cd30c4afe7b076bfa2883a47
Pietro Saccardi <lizardm4@gmail.com>
2015-08-16 14:17:58 +0200

    Separate class for track manipulation

--- elevation.py
+++ elevation.py
@@ -1,4 +1,103 @@
 import requests, time
+import xml.dom.minidom as DOM
+from datetime import datetime
+import dateutil
+
+class TrackPoint(object):
+	def __getattr__(self, key):
+		if key in ['lat', 'latitude']:
+			return float(self._dom_node.getAttribute('lat'))
+		elif key in ['lon', 'longitude']:
+			return float(self._dom_node.getAttribute('lon'))
+		elif key in ['elevation', 'ele']:
+			return self._get_elevation()
+		elif key == 'time':
+			return self._get_time()
+		else:
+			return super(TrackPoint, self).__getattr__(key)
+
+	def __setattr__(self, key, value):
+		if key in ['lat', 'latitude']:
+			self._dom_node.setAttribute('lat', ('%0.6f' % value))
+		elif key in ['lon', 'longitude']:
+			self._dom_node.setAttribute('lon', ('%0.6f' % value))
+		elif key in ['elevation', 'ele']:
+			self._set_elevation(value)
+		elif key == 'time':
+			self._set_time(value)
+		else:
+			super(TrackPoint, self).__setattr__(key, value)
+
+	def _ensure_tag(self, dom, tagname, default_value=''):
+		candidates = self._dom_node.getElementsByTagName(tagname)
+		if len(candidates) == 0:
+			node = dom.createElement(tagname)
+			node.appendChild(dom.createTextNode(default_value))
+			self._dom_node.appendChild(node)
+			return node
+		else:
+			node = candidates[0]
+			if node.hasChildNodes():
+				if node.firstChild.nodeType == DOM.TEXT_NODE:
+					default_value = node.firstChild.nodeValue
+					if node.firstChild == node.lastChild:
+						return node
+				while node.hasChildNodes():
+					node.removeChild(node.firstChild)
+			node.appendChild(dom.createTextNode(default_value))
+			return node
+
+	def _get_elevation(self):
+		str_value = self._dom_ele.firstChild.nodeValue
+		if str_value == '':
+			return None
+		else:
+			return float(str_value)
+	def _set_elevation(self, elevation):
+		print('setting elevation ' + str(elevation))
+		if elevation is None:
+			self._dom_ele.firstChild.nodeValue = ''
+		else:
+			self._dom_ele.firstChild.nodeValue = ('%0.2f' % elevation)
+
+	def _get_time(self):
+		str_value = self._dom_time.firstChild.nodeValue
+		if str_value == '':
+			return None
+		else:
+			return dateutil.parse(str_value)
+	def _set_time(self, dt):
+		if dt is None:
+			self._dom_time.firstChild.nodeValue = ''
+		else:
+			self._dom_time.firstChild.nodeValue = dt.isoformat()
+
+	def apiformat(self):
+		return '%0.6f,%0.6f' % (self.lat, self.lon)
+
+	def __init__(self, dom, trackpt_dom):
+		self._dom_node = trackpt_dom
+		self._dom_ele = self._ensure_tag(dom, 'ele')
+		self._dom_time = self._ensure_tag(dom, 'time')
+
+class TrackPoints(object):
+
+	def __getitem__(self, key):
+		return self._locations.__getitem__(key)
+	def __len__(self):
+		return len(self._locations)
+	def __iter__(self):
+		return self._locations.__iter__()
+	def __reversed__(self):
+		return self._locations.__reversed__()
+	def __contains__(self, item):
+		return self._locations.__contains__(item)
+
+	def __init__(self, dom):
+		self._locations = []
+		for trkpt in dom.getElementsByTagName('trkpt'):
+			self._locations.append(TrackPoint(dom, trkpt))
+
 
 class GoogleElevationAPI(requests.Session):
 	CHUNK_SIZE = 56
@@ -9,38 +108,55 @@ class GoogleElevationAPI(requests.Session):
 	STATUS_DENIED = 'REQUEST_DENIED'
 	STATUS_ERR = 'UNKNOWN_ERROR'
 
-	def _parse_result(self, json, idx=0):
-		self.status = json['status']
-		if self.status == GoogleElevationAPI.STATUS_OK:
+	def _parse_result(self, json):
+		self._last_status = json['status']
+		if self._last_status == GoogleElevationAPI.STATUS_OK:
 			for i in xrange(0, len(json['results'])):
-				lat, lon = self.locations[idx + i]
-				self.locations[idx + i] = lat, lon, json['results'][i]['elevation']
+				self._chunk[i].elevation = json['results'][i]['elevation']
 
-	def add_location(self, lat, lon):
-		self.locations.append((lat, lon))
-
-	def clear(self):
-		self.locations = []
-
-	def run(self, index_range=None, auto_sleep_quota=True):
+	def run(self, trackpts, auto_sleep_quota=True):
 		slept = False
-		if index_range is None:
-			index_range = (0, len(self.locations))
-		for start_idx in xrange(index_range[0], index_range[1], self.CHUNK_SIZE):
-			print('Processing nodes %d..%d (of %d)...' % (start_idx, start_idx + self.CHUNK_SIZE, len(self.locations)))
-			chunk = self.locations[start_idx : start_idx + self.CHUNK_SIZE]
-			self.params['locations'] = '|'.join([
-				('%0.6f,%0.6f' % item) for item in chunk
-			])
-			self._parse_result(self.get(self.ELEVATION_API_URL).json(), idx=start_idx)
-			if self.status != GoogleElevationAPI.STATUS_OK:
-				if auto_sleep_quota and self.status == STATUS_LIMIT_EXCEEDED and not slept:
-					# Sleep and try again
-					print('Quota exceeded. Sleeping 1 second.')
+		idx = 0
+		padding_d = '{:>' + str(len(str(len(trackpts)))) + 'd}'
+		format_str = 'Track points {0}..{0} (of {0}): querying {0} items...'.format(padding_d)
+		start_idx = -1
+		self._chunk = []
+		while idx < len(trackpts):
+			# if they're equal, the chunk has already been populated
+			if idx != start_idx:
+				start_idx = idx
+				self._chunk = []
+				# populate chunk
+				for trackpt in trackpts[start_idx:]:
+					idx += 1
+					if trackpt.elevation is None:
+						self._chunk.append(trackpt)
+						if len(self._chunk) >= self.CHUNK_SIZE:
+							break
+				# process chunk
+				print(format_str.format(start_idx, idx, len(trackpts), len(self._chunk)))
+				# populate params
+				self.params['locations'] = '|'.join([item.apiformat() for item in self._chunk])
+
+			response = self.get(self.ELEVATION_API_URL)
+			self._last_status = GoogleElevationAPI.STATUS_ERR
+			try:
+				self._parse_result(response.json())
+			except Exception, e:
+				print(str(e))
+				self._last_status = GoogleElevationAPI.STATUS_ERR
+			# handle errors
+			if self._last_status != GoogleElevationAPI.STATUS_OK:
+				if auto_sleep_quota and self._last_status == GoogleElevationAPI.STATUS_LIMIT_EXCEEDED \
+					and not slept:
+					# sleep and try again
+					print('Quota exceeded. Sleeping 1s.')
 					time.sleep(1)
 					slept = True
-					continue
+					idx = start_idx
+					continue # aka retry
 				else:
+					print('Status: %s. Aborting.' % self._last_status)
 					return False
 			else:
 				slept = False
@@ -51,6 +167,6 @@ class GoogleElevationAPI(requests.Session):
 		self.params = {
 			'key': api_key
 		}
-		self.locations = []
-		self.status = None
+		self._chunk = []
+		self._last_status = None
 
--- gpedit.py
+++ gpedit.py
@@ -3,66 +3,29 @@ import xml.dom.minidom as DOM
 import sys
 import os
 from elevation import GoogleElevationAPI as Elevation
+from elevation import TrackPoints
 
 API_KEY = 'YOUR_API_KEY'
 
-def dom_track_point_to_coordinates(trkpt):
-	lat = float(trkpt.getAttribute('lat'))
-	lon = float(trkpt.getAttribute('lon'))
-	ele = None
-	elevations = trkpt.getElementsByTagName('ele')
-	if len(elevations) > 0:
-		if elevations[0].hasChildNodes():
-			ele = float(elevations[0].firstChild.nodeValue)
-	return (lat, lon, ele)
-
-def dom_set_elevation(trkpt, elevation, dom):
-	ele = None
-	elevations = trkpt.getElementsByTagName('ele')
-	if len(elevations) > 0:
-		ele = elevations[0]
-	else:
-		ele = dom.createElement('ele')
-		trkpt.appendChild(ele)
-
-	while ele.hasChildNodes():
-		ele.removeChild(ele.firstChild)
-
-	ele.appendChild(dom.createTextNode('%0.2f' % elevation))
-
 def main(filename):
 	dom = DOM.parse(filename)
-	points = dom.getElementsByTagName('trkpt')
+	points = TrackPoints(dom)
 	elevapi = Elevation(API_KEY)
-	for trkpt in points:
-		lat, lon, _ = dom_track_point_to_coordinates(trkpt)
-		elevapi.add_location(lat, lon)
-	result = elevapi.run()
-	if not result:
-		print('Error. Google API returned: %s.' % elevapi.status)
-	for i in xrange(0, len(points)):
-		if len(elevapi.locations[i]) < 3:
-			print('Not all elevations retrieved.')
-			answer = None
-			while answer is None:
-				answer = raw_input('Proceed anyway? [y/N] ').lower()
-				if answer in ['y', 'n']:
-					if answer == 'y':
-						answer = True
-					else:
-						answer = False
-				else:
-					answer = None
-			if answer:
-				break
+	if not elevapi.run(points):
+		print('Not all elevations retrieved.')
+		answer = None
+		while answer is None:
+			answer = raw_input('Proceed anyway? [y/N] ').lower()
+			if answer in ['y', 'n']:
+				answer = answer == 'y'
 			else:
-				return
-		else:
-			dom_set_elevation(points[i], elevapi.locations[i][2], dom)
-	
+				answer = None
+		if not answer:
+			return
+
 	os.rename(filename, filename + '.bak')
 	with open(filename, 'w') as writer:
-		dom.writexml(writer, '', '  ', '')
+		dom.writexml(writer)
 
 if __name__ == '__main__':
 	if len(sys.argv) <= 1:
